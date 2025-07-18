d0f929fdc5369e42f211d5d0bf569b2bf7fb783b
ashiredev <219289962+ashiredev@users.noreply.github.com>
2025-07-18 19:28:59 +0200

    Add DE and WM name info

--- start-ade.sh
+++ start-ade.sh
@@ -2,6 +2,8 @@
 
 # Set x11 (or XLibre)
 export XDG_SESSION_TYPE=x11
+export XDG_CURRENT_DESKTOP=Ashire
+export DESKTOP_SESSION=Ashire
 
 # Start the Ashire window manager
 # ashire-window-manager &
